e9d9d5e5a54694f57dbdcc8b8fddf85f6923784a
JK <copy_x@naver.com>
2015-01-16 01:03:50 +0900

    Initial commit

---
+++ src/test/AccountTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Account;
+
+public class AccountTest {
+	
+	@Test
+	public void testAccount() throws Exception {
+		Account account = new Account(10000);
+	}
+	
+	@Test
+	public void testGetBalance() throws Exception {
+		Account account = new Account(10000);
+		assertEquals(10000, account.getBalance());
+
+		account = new Account(1000);
+		assertEquals(1000, account.getBalance());
+
+		account = new Account(0);
+		assertEquals(0, account.getBalance());
+	}
+}
